ac35f25c7487b8cb7a859a5eca60b9a3bb34548c
sebastianMindee <sebastian.oliverasilvera@mindee.co>
2023-11-14 19:05:24 +0100

    :building_construction: add geometry module

---
+++ src/geometry/MinMax.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Mindee\geometry;
+
+class MinMax
+{
+    private float $min;
+    private float $max;
+
+    public function __construct(float $min, float $max)
+    {
+        $this->min = $min;
+        $this->max = $max;
+    }
+
+    public function getMin()
+    {
+        return $this->min;
+    }
+
+    public function getMax()
+    {
+        return $this->max;
+    }
+}
