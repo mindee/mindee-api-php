6e6400596e1afe4e185aef8eaf5a8f256b534d36
sebastianMindee <sebastian.oliverasilvera@mindee.co>
2023-11-15 18:22:17 +0100

    :building_construction: add input source module

--- src/input/PageOptions.php
+++ src/input/PageOptions.php
@@ -2,6 +2,22 @@
 
 namespace Mindee\input;
 
+const KEEP_ONLY = 'KEEP_ONLY';
+const REMOVE = 'REMOVE';
+
 class PageOptions
 {
+    public array $pageIndexes;
+    public string $operation;
+    public int $onMinPage;
+
+    public function __construct(
+        ?array $pageIndexes = null,
+        string $operation = KEEP_ONLY,
+        int $onMinPage = 0)
+    {
+        $this->pageIndexes = $pageIndexes;
+        $this->operation = $operation;
+        $this->onMinPage = $onMinPage;
+    }
 }
