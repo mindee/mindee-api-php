794a26080d5ec6beee4c19eed7b417e23e4d1553
sebastianMindee <sebastian.oliverasilvera@mindee.co>
2023-11-20 11:04:38 +0100

    :art: add unit test setup

--- src/parsing/common/PredictResponse.php
+++ src/parsing/common/PredictResponse.php
@@ -6,7 +6,7 @@ class PredictReponse extends ApiResponse
 {
     public Document $document;
 
-    public function __construct(Inference $prediction_type, array $raw_response)
+    public function __construct(string $prediction_type, array $raw_response)
     {
         parent::__construct($raw_response);
         $this->document = new Document($prediction_type, $raw_response['document']);
