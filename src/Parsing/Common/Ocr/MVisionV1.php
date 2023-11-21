d44fee07cd0aeb5e30a996116b62fa887a2a4cf1
sebastianMindee <sebastian.oliverasilvera@mindee.co>
2023-11-21 11:29:49 +0100

    :sparkles: add support for Ocr

---
+++ src/Parsing/Common/Ocr/MVisionV1.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Mindee\Parsing\Common\Ocr;
+
+class MVisionV1
+{
+    public array $pages;
+
+    function __construct(array $raw_prediction)
+    {
+        $this->pages = [];
+        foreach ($raw_prediction['pages'] as $page_prediction) {
+            $this->pages[] = new OcrPage($page_prediction);
+        }
+    }
+
+    public function __toString(): string
+    {
+        $pages_str = [];
+        foreach ($this->pages as $page) {
+            $pages_str[] = strval($page);
+        }
+        return implode("\n", $pages_str);
+    }
+}
