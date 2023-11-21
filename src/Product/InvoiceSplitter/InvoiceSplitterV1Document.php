23c763a648b8c27ebf07e4412c3714798ee8a81d
sebastianMindee <sebastian.oliverasilvera@mindee.co>
2023-11-21 16:37:56 +0100

    :sparkles: add support for async & InvoiceSplitterV1

---
+++ src/Product/InvoiceSplitter/InvoiceSplitterV1Document.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Mindee\Product\InvoiceSplitter;
+
+use Mindee\Parsing\Common\Prediction;
+
+/**
+ * Document data for Invoice Splitter, API version 1.
+ */
+class InvoiceSplitterV1Document extends Prediction
+{
+    /**
+     * Page groups linked to an invoice.
+     */
+    public array $invoicePageGroups;
+
+    function __construct(array $raw_prediction, ?int $page_id = null)
+    {
+        $this->invoicePageGroups = [];
+        if (array_key_exists("invoice_page_groups", $raw_prediction)) {
+            foreach ($raw_prediction['invoice_page_groups'] as $prediction) {
+                $this->invoicePageGroups[] = new InvoiceSplitterV1PageGroup($prediction);
+            }
+        }
+    }
+
+    function __toString(): string
+    {
+        $out_str = ":Invoice Page Groups:";
+        foreach ($this->invoicePageGroups as $pageGroup) {
+            $out_str .= "\n  $pageGroup";
+        }
+        return trim($out_str);
+    }
+}
+
