f7db16d75e9bc129c2ff6ee0d7d9eb8668ec8a4a
sebastianMindee <sebastian.oliverasilvera@mindee.co>
2023-12-14 15:53:32 +0100

    fix cropper extras, update tests

--- src/Parsing/Common/Extras/Extras.php
+++ src/Parsing/Common/Extras/Extras.php
@@ -25,7 +25,7 @@ class Extras
      * Sets a field.
      *
      * @param mixed $varName Name of the field to set.
-     * @param mixed $value   Value to set the field with.
+     * @param mixed $value Value to set the field with.
      * @return void
      */
     public function __set($varName, $value)
@@ -42,7 +42,7 @@ class Extras
             if ($key != 'cropper') {
                 $this->__set($key, $extra);
             } else {
-                $this->__set('cropper', new CropperExtra($rawPrediction['cropper']));
+                $this->cropper = new CropperExtra($rawPrediction['cropper']);
             }
         }
     }
--- tests/Parsing/Common/Extras/CropperExtraTest.php
+++ tests/Parsing/Common/Extras/CropperExtraTest.php
@@ -2,13 +2,43 @@
 
 namespace Parsing\Common\Extras;
 
+use Mindee\Input\PredictOptions;
+use Mindee\Parsing\Common\Document;
 use Mindee\Parsing\Common\Extras\CropperExtra;
+use Mindee\Client;
+use Mindee\Product\Receipt\ReceiptV5;
 use PHPUnit\Framework\TestCase;
 
-class CropperExtraTest extends TestCase //TODO: complete when receipt is implemented.
+class CropperExtraTest extends TestCase
 {
-    public function testPlaceHolderForCI()
+    private string $cropperDir;
+    private Document $completeDoc;
+    protected function setUp(): void
     {
-        $this->assertTrue(true);
+        $this->cropperDir = (
+            getenv('GITHUB_WORKSPACE') ?: "."
+            ) . "/tests/resources/extras/cropper/";
+        $completeDocFile = file_get_contents($this->cropperDir . "complete.json");
+        $completeDocJSON = json_decode($completeDocFile, true);
+        $this->completeDoc = new Document(ReceiptV5::class, $completeDocJSON["document"]);
+    }
+    public function testEnqueuingCropperEnqueuesCropper()
+    {
+        $predictOptions = new PredictOptions();
+        $predictOptions->setCropper(true);
+        $this->assertTrue($predictOptions->cropper);
+    }
+
+    public function testCropperExtra()
+    {
+        $this->assertEquals(1, count($this->completeDoc->inference->pages[0]->extras->cropper->croppings));
+        $this->assertEquals(0.057, $this->completeDoc->inference->pages[0]->extras->cropper->croppings[0]->boundingBox->getCoordinates()[0]->getX());
+        $this->assertEquals(0.008, $this->completeDoc->inference->pages[0]->extras->cropper->croppings[0]->boundingBox->getCoordinates()[0]->getY());
+        $this->assertEquals(0.846, $this->completeDoc->inference->pages[0]->extras->cropper->croppings[0]->boundingBox->getCoordinates()[1]->getX());
+        $this->assertEquals(0.008, $this->completeDoc->inference->pages[0]->extras->cropper->croppings[0]->boundingBox->getCoordinates()[1]->getY());
+        $this->assertEquals(0.846, $this->completeDoc->inference->pages[0]->extras->cropper->croppings[0]->boundingBox->getCoordinates()[2]->getX());
+        $this->assertEquals(1.0, $this->completeDoc->inference->pages[0]->extras->cropper->croppings[0]->boundingBox->getCoordinates()[2]->getY());
+        $this->assertEquals(0.057, $this->completeDoc->inference->pages[0]->extras->cropper->croppings[0]->boundingBox->getCoordinates()[3]->getX());
+        $this->assertEquals(1.0, $this->completeDoc->inference->pages[0]->extras->cropper->croppings[0]->boundingBox->getCoordinates()[3]->getY());
     }
 }
