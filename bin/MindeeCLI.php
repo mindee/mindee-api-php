3c8ac8da6b425890b7699a28e5b5acd1e934d826
sebastianMindee <sebastian.oliverasilvera@mindee.co>
2024-01-05 18:13:41 +0100

    switch CLI tools & move files

--- bin/MindeeCLI.php
+++ bin/MindeeCLI.php
@@ -3,128 +3,107 @@
 namespace Mindee\CLI;
 
 require __DIR__ . '/../vendor/autoload.php';
-require __DIR__ . '/vendor/autoload.php';
 require __DIR__ . '/../src/version.php';
 
-use splitbrain\phpcli\CLI;
-use splitbrain\phpcli\Options;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
 
 use const Mindee\VERSION;
 
-class MindeeCLI extends CLI
+class MindeeCLI extends Command
 {
     private array $documentList;
     private ?string $apiKey;
     private ?string $cutPages;
     private ?string $keepPages;
 
-    protected function registerMainOptions(Options $options)
+    public function __construct(array $documentList)
     {
-        $options->registerCommand("parse", "Parses a document.");
-        $options->registerOption('input-type', 'Input Type', 'i', "input_type");//TODO add values in post
-        $options->registerArgument(
-            'input_type',
-            "Specify how to handle the input.\n" .
-            "- path: open a path (default).\n" .
-            "- file: open as a file handle.\n" .
-            "- base64: open a base64 encoded text file.\n" .
-            "- bytes: open the contents as raw bytes.\n" .
-            "- url: open an URL.",
-            false,
-        );
-        $options->registerOption("cut-doc", "Cut Document pages", "c", "cut_doc");
-        $options->registerArgument("cut_doc", "Cuts to apply to document", true, "parse");
-        $options->registerOption("pages-keep", "Number of document pages to keep, default: 5.", "p", "pages_keep");
-        $options->registerArgument("pages_keep", "Pages to keep, default: 5.", true, "parse");
-
-        $options->registerOption("key", "API key for the account. Is retrieved from environment if not provided.", "k", "api_key");
-        $options->registerArgument("api_key", "API key.", false);
-    }
-
-    protected function registerCustomOptions(Options $options)
-    {
-        $options->registerOption("account", "API account name for the endpoint", "a", "account_name", "custom");
-        $options->registerArgument("account_name", "Account name.", true, "custom");
-
-        $options->registerOption("endpoint", "API endpoint name for the endpoint", "e", "endpoint_name", "custom");
-        $options->registerArgument("endpoint_name", "Endpoint name.", true, "custom");
-
-        $options->registerOption(
-            "version",
-            "Version for the endpoint. If not set, use the latest version of the model.",
-            "v",
-            "endpoint_version",
-            "custom"
-        );
-        $options->registerArgument("endpoint_version", "Endpoint version.", false, "custom");
-    }
-
-    protected function registerOTSOptions(Options $options, string $commandName, CommandConfig $commandConfig)
-    {
-        $options->registerOption("full-text", "Include full document text in response.", "t", false, $commandName);
-        if ($commandConfig->isAsync && $commandConfig->isSync) {
-            $options->registerOption("asynchronous", "Parse asynchronously.", "A", false, $commandName);
-        }
-    }
-
-    protected function registerDisplayOptions(Options $options)
-    {
-        $options->registerOption('output-type', 'Output Type', 'o', "output_type");//TODO add values in post
-        $options->registerArgument(
-            'output_type',
-            "Specify how to output the data.\n" .
-            "- summary: a basic summary (default)\n" .
-            "- raw: the raw HTTP response\n" .
-            "- parsed: the validated and parsed data fields\n",
-            false,
-            "parse"
-        );
-    }
-
-    protected function setup(Options $options)
-    {
-        $options->setHelp('Mindee client.');
-        $this->registerMainOptions($options);
-        $this->registerDisplayOptions($options);
-        foreach ($this->documentList as $documentName => $documentAttributes) {
-            $options->registerCommand($documentName, $documentAttributes->help);
-            if ($documentAttributes->isSync) {
-                if ($documentName == "custom") {
-                    $this->registerCustomOptions($options);
-                } else {
-                    $this->registerOTSOptions($options, $documentName, $documentAttributes);
-                }
-            }
-        }
-    }
-
-    protected function callParse(){
-
-    }
-
-    // implement your code
-    protected function main(Options $options)
-    {
-        $options->useCompactHelp();
-        if ($options->getOpt('version')) {
-            $this->info(VERSION);
-        } elseif ($options->getOpt('help')) {
-            echo $options->help();
-        } else {
-            if($options->getOpt('key')){
-                $this->apiKey = $options->getOpt('key');
-            }
-            if($options->getOpt('cut-doc')){
-                $this->apiKey = $options->getOpt('cut-doc');
-            }
-        }
-    }
-
-    public function __construct(
-        array $documentList
-    ) {
-        parent::__construct(true);
+        parent::__construct();
         $this->apiKey = null;
         $this->documentList = $documentList;
+        $this->setHelp($this->formatHelp());
+    }
+
+    protected function formatHelp()
+    {
+        $helpCondensed = "Mindee Command-Line interface.
+Usage:
+  mindee product [options]
+
+Arguments:";
+        foreach ($this->documentList as $documentName => $document) {
+            $helpCondensed .= "\n  " . str_pad($documentName, 65 - strlen($document->help)) . $document->help;
+        }
+        return $helpCondensed;
+    }
+
+    protected function configure()
+    {
+        $this
+            ->setName('mindee')
+            ->setDescription('Mindee client.');
+
+        $this->configureMainOptions();
+        $this->configureCustomOptions();
+        // Add more configuration if needed for OTS options and display options
+    }
+
+    private function configureMainOptions()
+    {
+        $this
+            ->addArgument('input_type', InputArgument::OPTIONAL, 'Specify how to handle the input.')
+            ->addOption('cut_doc', 'c', InputOption::VALUE_REQUIRED, 'Cuts to apply to document')
+            ->addOption('pages_keep', 'p', InputOption::VALUE_REQUIRED, 'Pages to keep, default: 5')
+            ->addOption('key', 'k', InputOption::VALUE_OPTIONAL, 'API key for the account. Is retrieved from environment if not provided.');
+    }
+
+    private function configureCustomOptions()
+    {
+        $this
+            ->addOption('account_name', 'a', InputOption::VALUE_REQUIRED, 'API account name for the endpoint')
+            ->addOption('endpoint_name', 'e', InputOption::VALUE_REQUIRED, 'API endpoint name for the endpoint')
+            ->addOption('endpoint_version', 'd', InputOption::VALUE_OPTIONAL, 'Version for the endpoint. If not set, use the latest version of the model');
+    }
+
+    // Implement your logic here
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $version = $input->getOption('version');
+        // Check if no arguments are provided
+        if (!isset($input->getArguments()["input_type"]) || is_null($input->getArguments()["input_type"])) {
+            $output->writeln($this->getApplication()->find('mindee')->getHelp());
+            return Command::SUCCESS;
+        } else {
+            foreach ($input->getArguments() as $ipt) {
+                $output->writeln($ipt);
+            }
+            $output->writeln(sizeof($input->getArguments()));
+
+        }
+        if ($version) {
+            $output->writeln(VERSION);
+        } else {
+            $key = $input->getOption('key');
+            $cutDoc = $input->getOption('cut_doc');
+            // Handle other options similarly
+
+            $output->writeln('This is the output of the command');
+            // Your logic goes here based on the input options
+            return Command::SUCCESS;
+        }
     }
 }
+
+// References for later:
+// Options:
+//  -c, --cut_doc CUT_DOC Specify how to handle the input.
+//  -p, --pages_keep PAGES_KEEP
+//  -k, --key [KEY]
+//  -a, --account_name ACCOUNT_NAME
+//  -e, --endpoint_name ENDPOINT_NAME
+//  -d, --endpoint_version [ENDPOINT_VERSION] [--]
+//  -A, --async Parses asynchronously
