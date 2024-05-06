15d0f88c11f9d8b9d8ede464e4a68658296da701
sebastianMindee <sebastian.oliverasilvera@mindee.co>
2024-05-06 15:31:04 +0200

    add test - marked as regression for CI purposes

---
+++ tests/Input/URLInputSourceTestRegression.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Input;
+
+use Mindee\Client;
+use Mindee\Parsing\Common\PredictResponse;
+use Mindee\Product\Invoice\InvoiceV4;
+use PHPUnit\Framework\TestCase;
+
+class URLInputSourceTestRegression extends TestCase
+{
+
+    protected string $fileTypesDir;
+
+    public function setUp(): void
+    {
+        $this->realClient = new Client();
+    }
+
+    public function testSendUrlInputSource()
+    {
+        $urlInputSource = $this->realClient->sourceFromUrl("https://raw.githubusercontent.com/mindee/client-lib-test-data/main/file_types/pdf/blank_1.pdf");
+        $res = $this->realClient->parse(InvoiceV4::class, $urlInputSource);
+        $this->assertInstanceOf(PredictResponse::class, $res);
+    }
+}
