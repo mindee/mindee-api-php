83d3a917dd4463c8265f8543c8f330f727d15918
sebastianMindee <sebastian.oliverasilvera@mindee.co>
2024-08-29 14:49:34 +0200

    add imagemagick & gs dependency test

---
+++ tests/Parsing/DependencyCheckerTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Parsing;
+
+use Mindee\Parsing\DependencyChecker;
+use PHPUnit\Framework\TestCase;
+
+class DependencyCheckerTest extends TestCase {
+    public function testGhostScriptDependency() {
+        $this->assertTrue(DependencyChecker::isGhostscriptAvailable());
+    }
+
+    public function testImageMagickDependency() {
+        $this->assertTrue(DependencyChecker::isImageMagickAvailable());
+    }
+
+    public function testImageMagickPolicy() {
+        $this->assertTrue(DependencyChecker::isImageMagickAvailable());
+    }
+}
