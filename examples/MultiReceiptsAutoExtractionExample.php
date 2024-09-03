f24dff8df2cb52912c4cba037f1324947739460e
sebastianMindee <sebastian.oliverasilvera@mindee.co>
2024-09-03 12:06:24 +0200

    added (untested) examples

---
+++ examples/MultiReceiptsAutoExtractionExample.php
@@ -0,0 +1,36 @@
+<?php
+
+use Mindee\Client;
+use Mindee\Extraction\ImageExtractor;
+use Mindee\Input\PathInput;
+use Mindee\Product\MultiReceiptsDetector\MultiReceiptsDetectorV1;
+use Mindee\Product\Receipt\ReceiptV5;
+
+$apiKey = getenv("MINDEE_API_KEY");
+$mindeeClient = new Client($apiKey);
+
+$inputPath = "path/to/your/file.ext";
+$inputSource = new PathInput($inputPath);
+
+$imageExtractor = new ImageExtractor($inputSource);
+
+$multiReceiptsResult = $mindeeClient->parse(MultiReceiptsDetectorV1::class, $inputSource);
+$pageCount = 1;
+
+if ($inputSource->isPDF()) {
+    $pageCount = $inputSource->countDocPages();
+}
+
+$totalExtractedReceipts = [];
+
+for ($i = 0; $i < $pageCount; $i++) {
+    $receiptsPositions = $multiReceiptsResult->document->inference->pages[$i]->prediction->receipts;
+    $extractedReceipts = $imageExtractor->extractImagesFromPage($receiptsPositions, $i);
+    $totalExtractedReceipts = array_merge($totalExtractedReceipts, $extractedReceipts);
+}
+
+foreach ($totalExtractedReceipts as $receipt) {
+    // $receipt->writeToFile("output/path"); // Optional: save the extracted receipts to a file.
+    $result = $mindeeClient->parse(ReceiptV5::class, $receipt->asInputSource());
+    echo $result->document;
+}
