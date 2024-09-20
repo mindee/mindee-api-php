3eb357fcc76769e33696aef745437038e1d54964
sebastianMindee <sebastian.oliverasilvera@mindee.co>
2024-09-20 10:56:53 +0200

    commented testing utilities & made things more legible

--- tests/TestingUtilities.php
+++ tests/TestingUtilities.php
@@ -1,9 +1,17 @@
 <?php
 
-
+/**
+ * Testing utility class.
+ */
 class TestingUtilities
 {
-    public static function getVersion(string $rstStr)
+    /**
+     * Retrieves the version in a string of a prediction's RST.
+     *
+     * @param string $rstStr The input string.
+     * @return string The version present in the string.
+     */
+    public static function getVersion(string $rstStr): string
     {
         $versionLineStartPos = mb_strpos($rstStr, ":Product: ", 0, "UTF-8");
         $versionEndPos = mb_strpos($rstStr, "\n", $versionLineStartPos, "UTF-8");
@@ -15,35 +23,64 @@ class TestingUtilities
     }
 
 
-    public static function getId($rstStr)
+    /**
+     * Retrieves the ID in a string of a prediction's RST.
+     *
+     * @param string $rstStr The input string.
+     * @return string The ID present in the string.
+     */
+    public static function getId(string $rstStr): string
     {
-        // Replaces the string of a created object to avoid errors during tests.
-
         $idEndPos = mb_strpos($rstStr, "\n:Filename:", 0, "UTF-8");
         $idStartPos = mb_strpos($rstStr, ":Mindee ID: ", 0, "UTF-8");
 
         return mb_substr($rstStr, $idStartPos + 12, $idEndPos - ($idStartPos + 12), "UTF-8");
     }
 
-    private static function lev($s, $t)
+    /**
+     * Levenshtein distance function.
+     * Taken from:
+     * https://stackoverflow.com/questions/52201109/why-does-the-function-levenshtein-in-php-have-a-255-character-limit
+     * Which in turn was taken from:
+     * https://en.wikibooks.org/wiki/Algorithm_Implementation/Strings/Levenshtein_distance#PHP
+     * @param string $referenceStr Base string.
+     * @param string $targetStr    String to compare.
+     * @return float Levenshtein distance between the two strings.
+     */
+    private static function lev(string $referenceStr, string $targetStr): float
     {
-        $m = strlen($s);
-        $n = strlen($t);
+        $refLength = strlen($referenceStr);
+        $targetLength = strlen($targetStr);
 
-        for ($i = 0; $i <= $m; $i++) $d[$i][0] = $i;
-        for ($j = 0; $j <= $n; $j++) $d[0][$j] = $j;
+        for ($i = 0; $i <= $refLength; $i++) {
+            $distanceTable[$i][0] = $i;
+        }
+        for ($j = 0; $j <= $targetLength; $j++) {
+            $distanceTable[0][$j] = $j;
+        }
 
-        for ($i = 1; $i <= $m; $i++) {
-            for ($j = 1; $j <= $n; $j++) {
-                $c = ($s[$i - 1] == $t[$j - 1]) ? 0 : 1;
-                $d[$i][$j] = min($d[$i - 1][$j] + 1, $d[$i][$j - 1] + 1, $d[$i - 1][$j - 1] + $c);
+        for ($i = 1; $i <= $refLength; $i++) {
+            for ($j = 1; $j <= $targetLength; $j++) {
+                $c = ($referenceStr[$i - 1] == $targetStr[$j - 1]) ? 0 : 1;
+                $distanceTable[$i][$j] = min(
+                    $distanceTable[$i - 1][$j] + 1,
+                    $distanceTable[$i][$j - 1] + 1,
+                    $distanceTable[$i - 1][$j - 1] + $c
+                );
             }
         }
 
-        return $d[$m][$n];
+        return $distanceTable[$refLength][$targetLength];
     }
 
-    public static function levenshteinRatio(string $ref, string $target)
+    /**
+     * Computes the levenshtein ratio between two strings.
+     *
+     * @param string $ref    Reference string.
+     * @param string $target Target string.
+     * @return float The levenshtein ratio.
+     */
+    public static function levenshteinRatio(string $ref, string $target): float
     {
         $lev = TestingUtilities::lev($ref, $target);
         $maxLen = max(strlen($ref), strlen($target));
