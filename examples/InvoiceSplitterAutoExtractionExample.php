9e22dc1f635bb4a2732dab23b40720bad66d2510
sebastianMindee <sebastian.oliverasilvera@mindee.co>
2024-09-25 14:47:31 +0200

    :memo: fix example files

--- examples/InvoiceSplitterAutoExtractionExample.php
+++ examples/InvoiceSplitterAutoExtractionExample.php
@@ -3,40 +3,50 @@
 use Mindee\Client;
 use Mindee\Extraction\PdfExtractor;
 use Mindee\Input\PathInput;
-use Mindee\Product\InvoiceSplitter\InvoiceSplitterV1;
 use Mindee\Product\Invoice\InvoiceV4;
+use Mindee\Product\InvoiceSplitter\InvoiceSplitterV1;
 
-$apiKey = getenv("MINDEE_API_KEY");
-$mindeeClient = new Client($apiKey);
+function parseInvoice(string $filePath, Client $mindeeClient)
+{
+    $inputSource = new PathInput($filePath);
 
-$inputPath = "path/to/your/file.ext";
-$inputSource = new PathInput($inputPath);
-
-if ($inputSource->isPdf()) {
-    $pdfExtractor = new PdfExtractor($inputSource);
-    if ($pdfExtractor->getPageCount() > 1) {
-        $invoiceSplitterResponse = $mindeeClient->enqueueAndParse(
-            InvoiceSplitterV1::class,
-            $inputSource,
-        );
-        $pageGroups = $invoiceSplitterResponse->document->inference->prediction->invoicePageGroups;
-        $extractedPdfs = $pdfExtractor->extractInvoices($pageGroups);
-
-        foreach ($extractedPdfs as $extractedPdf) {
-            // Optional: Save the files locally
-            // $extractedPdf->writeToFile("output/path"):
-
-            $invoiceResult = $mindeeClient->parse(
-                InvoiceV4::class,
-                $extractedPdf->asInputSource()
-            );
-            echo $invoiceResult->document;
-        }
+    if ($inputSource->isPdf() && $inputSource->countDocPages() > 1) {
+        parseMultiPagePdf($inputSource, $mindeeClient);
     } else {
-        $invoiceResult = $mindeeClient->parse(InvoiceV4::class, $inputSource);
-        echo $invoiceResult->document;
+        parseSinglePageOrNonPdf($inputSource, $mindeeClient);
     }
-} else {
+}
+
+function parseSinglePageOrNonPdf(PathInput $inputSource, Client $mindeeClient)
+{
     $invoiceResult = $mindeeClient->parse(InvoiceV4::class, $inputSource);
     echo $invoiceResult->document;
 }
+
+function parseMultiPagePdf(PathInput $inputSource, Client $mindeeClient)
+{
+    global $mindeeClient;
+    $pdfExtractor = new PdfExtractor($inputSource);
+    $invoiceSplitterResponse = $mindeeClient->enqueueAndParse(
+        InvoiceSplitterV1::class,
+        $inputSource
+    );
+    $pageGroups = $invoiceSplitterResponse->document->inference->prediction->invoicePageGroups;
+    $extractedPdfs = $pdfExtractor->extractInvoices($pageGroups);
+
+    foreach ($extractedPdfs as $extractedPdf) {
+        // Optional: Save the files locally
+        // $extractedPdf->writeToFile("output/path");
+
+        $invoiceResult = $mindeeClient->parse(
+            InvoiceV4::class,
+            $extractedPdf->asInputSource()
+        );
+        echo $invoiceResult->document;
+    }
+}
+
+$mindeeClient = new Client("my-api-key-here");
+// $mindeeClient = new Client(); // Optionally, use an environment variable.
+$inputPath = "path/to/your/file.ext";
+parseInvoice($inputPath, $mindeeClient);
--- examples/MultiReceiptsAutoExtractionExample.php
+++ examples/MultiReceiptsAutoExtractionExample.php
@@ -6,31 +6,32 @@ use Mindee\Input\PathInput;
 use Mindee\Product\MultiReceiptsDetector\MultiReceiptsDetectorV1;
 use Mindee\Product\Receipt\ReceiptV5;
 
-$apiKey = getenv("MINDEE_API_KEY");
-$mindeeClient = new Client($apiKey);
+function processReceipts($client, $inputPath) {
+    $inputSource = new PathInput($inputPath);
+    $imageExtractor = new ImageExtractor($inputSource);
 
-$inputPath = "path/to/your/file.ext";
-$inputSource = new PathInput($inputPath);
-
-$imageExtractor = new ImageExtractor($inputSource);
-
-$multiReceiptsResult = $mindeeClient->parse(MultiReceiptsDetectorV1::class, $inputSource);
-$pageCount = 1;
-
-if ($inputSource->isPDF()) {
+    $multiReceiptsResult = $client->parse(MultiReceiptsDetectorV1::class, $inputSource);
     $pageCount = $inputSource->countDocPages();
+
+    $totalExtractedReceipts = [];
+
+    for ($i = 0; $i < $pageCount; $i++) {
+        $receiptsPositions = $multiReceiptsResult->document->inference->pages[$i]->prediction->receipts;
+        $extractedReceipts = $imageExtractor->extractImagesFromPage($receiptsPositions, $i);
+        $totalExtractedReceipts = array_merge($totalExtractedReceipts, $extractedReceipts);
+    }
+
+    foreach ($totalExtractedReceipts as $receipt) {
+        // Optional: save the extracted receipts to a file
+        // $receipt->writeToFile("output/path");
+
+        $result = $client->parse(ReceiptV5::class, $receipt->asInputSource());
+        echo $result->document . "\n";
+    }
 }
 
-$totalExtractedReceipts = [];
+$mindeeClient = new Client("my-api-key-here");
+// $mindeeClient = new Client(); // Optionally, use an environment variable.
+$inputPath = "path/to/your/file.ext";
 
-for ($i = 0; $i < $pageCount; $i++) {
-    $receiptsPositions = $multiReceiptsResult->document->inference->pages[$i]->prediction->receipts;
-    $extractedReceipts = $imageExtractor->extractImagesFromPage($receiptsPositions, $i);
-    $totalExtractedReceipts = array_merge($totalExtractedReceipts, $extractedReceipts);
-}
-
-foreach ($totalExtractedReceipts as $receipt) {
-    // $receipt->writeToFile("output/path"); // Optional: save the extracted receipts to a file.
-    $result = $mindeeClient->parse(ReceiptV5::class, $receipt->asInputSource());
-    echo $result->document;
-}
+processReceipts($mindeeClient, $inputPath);
