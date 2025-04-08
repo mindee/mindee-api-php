1508c317cff3320e66a55a9a6cb7a4266cb0c77c
sebastianMindee <sebastian.oliverasilvera@mindee.co>
2025-04-08 17:44:54 +0200

    :sparkles: add ocr line extraction from regex

--- src/Parsing/Common/Ocr/Ocr.php
+++ src/Parsing/Common/Ocr/Ocr.php
@@ -2,6 +2,8 @@
 
 namespace Mindee\Parsing\Common\Ocr;
 
+use function Sodium\add;
+
 /**
  * OCR extraction from the entire document.
  */
@@ -26,5 +28,28 @@ class Ocr
     public function __toString(): string
     {
         return strval($this->mvisionV1);
+    }
+
+    /**
+     * Finds all lines matching the given regex in the OCR data, indexed by their page.
+     *
+     * @param string $regex The regular expression to match against.
+     * @return array All lines that match the regex, indexed by their page.
+     */
+    public function findLineByRegex(string $regex): array
+    {
+        $matches = [];
+        for ($i = 0; $i < count($this->mvisionV1->pages); $i++) {
+            $page = $this->mvisionV1->pages[$i];
+            foreach ($page->getAllLines() as $line) {
+                if (preg_match($regex, strval($line))) {
+                    if (!array_key_exists($i, $matches)) {
+                        $matches[$i] = [];
+                    }
+                    $matches[$i][] = $line;
+                }
+            }
+        }
+        return $matches;
     }
 }
