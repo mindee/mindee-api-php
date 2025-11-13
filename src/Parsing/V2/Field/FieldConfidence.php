21084393c72db5dbdcfd1c29989c35f25f87f212
sebastianMindee <130448732+sebastianMindee@users.noreply.github.com>
2025-11-13 15:39:31 +0100

    :sparkles: add confidence scores comparison operators (#155)

--- src/Parsing/V2/Field/FieldConfidence.php
+++ src/Parsing/V2/Field/FieldConfidence.php
@@ -8,4 +8,69 @@ enum FieldConfidence: string
     case High = 'High';
     case Medium = 'Medium';
     case Low = 'Low';
+
+    /**
+     * @return integer Rank of the value.
+     */
+    public function rank(): int
+    {
+        return match ($this) {
+            self::Low => 1,
+            self::Medium => 2,
+            self::High => 3,
+            self::Certain => 4,
+        };
+    }
+
+    /**
+     * Shorthand for the '<=' operator.
+     * @param FieldConfidence $other Other confidence value.
+     * @return boolean True if this confidence is lower than or equal to the other.
+     */
+    public function lte(FieldConfidence $other): bool
+    {
+        return $this->rank() <= $other->rank();
+    }
+
+    /**
+     * Shorthand for the '>=' operator.
+     * @param FieldConfidence $other Other confidence value.
+     * @return boolean True if this confidence is greater than or equal to the other.
+     */
+    public function gte(FieldConfidence $other): bool
+    {
+        return $this->rank() >= $other->rank();
+    }
+
+    /**
+     * Shorthand for the '<' operator.
+     * @param FieldConfidence $other Other confidence value.
+     * @return boolean True if this confidence is lower than the other.
+     */
+    public function lt(FieldConfidence $other): bool
+    {
+        return $this->rank() < $other->rank();
+    }
+
+
+    /**
+     * Shorthand for the '>' operator.
+     * @param FieldConfidence $other Other confidence value.
+     * @return boolean True if this confidence is greater than the other.
+     */
+    public function gt(FieldConfidence $other): bool
+    {
+        return $this->rank() > $other->rank();
+    }
+
+
+    /**
+     * Shorthand for the '==' operator.
+     * @param FieldConfidence $other Other confidence value.
+     * @return boolean True if this confidence is equal to the other.
+     */
+    public function eq(FieldConfidence $other): bool
+    {
+        return $this === $other;
+    }
 }
