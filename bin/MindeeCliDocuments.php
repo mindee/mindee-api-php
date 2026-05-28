acd3e8ad03490f8fca469b7df4082ab83a1022cf
sebastianMindee <130448732+sebastianMindee@users.noreply.github.com>
2026-05-28 11:24:06 +0200

    :coffin: remove support for unused V1 products

--- bin/MindeeCliDocuments.php
+++ bin/MindeeCliDocuments.php
@@ -5,31 +5,22 @@ declare(strict_types=1);
 namespace Mindee\Cli;
 
 use Mindee\V1\Product\BarcodeReader\BarcodeReaderV1;
-use Mindee\V1\Product\BillOfLading\BillOfLadingV1;
 use Mindee\V1\Product\BusinessCard\BusinessCardV1;
 use Mindee\V1\Product\Cropper\CropperV1;
-use Mindee\V1\Product\DeliveryNote\DeliveryNoteV1;
 use Mindee\V1\Product\DriverLicense\DriverLicenseV1;
 use Mindee\V1\Product\FinancialDocument\FinancialDocumentV1;
 use Mindee\V1\Product\Fr\BankAccountDetails\BankAccountDetailsV2;
 use Mindee\V1\Product\Fr\CarteGrise\CarteGriseV1;
-use Mindee\V1\Product\Fr\EnergyBill\EnergyBillV1;
-use Mindee\V1\Product\Fr\HealthCard\HealthCardV1;
 use Mindee\V1\Product\Fr\IdCard\IdCardV2;
-use Mindee\V1\Product\Fr\Payslip\PayslipV3;
 use Mindee\V1\Product\Generated\GeneratedV1;
-use Mindee\V1\Product\Ind\IndianPassport\IndianPassportV1;
 use Mindee\V1\Product\InternationalId\InternationalIdV2;
 use Mindee\V1\Product\Invoice\InvoiceV4;
 use Mindee\V1\Product\InvoiceSplitter\InvoiceSplitterV1;
 use Mindee\V1\Product\MultiReceiptsDetector\MultiReceiptsDetectorV1;
-use Mindee\V1\Product\NutritionFactsLabel\NutritionFactsLabelV1;
 use Mindee\V1\Product\Passport\PassportV1;
 use Mindee\V1\Product\Receipt\ReceiptV5;
 use Mindee\V1\Product\Resume\ResumeV1;
 use Mindee\V1\Product\Us\BankCheck\BankCheckV1;
-use Mindee\V1\Product\Us\HealthcareCard\HealthcareCardV1;
-use Mindee\V1\Product\Us\UsMail\UsMailV3;
 
 /**
  * Document specifications for CLI usage.
@@ -55,12 +46,6 @@ class MindeeCliDocuments
                 true,
                 false
             ),
-            "bill-of-lading" => new DocumentCommandConfig(
-                "Bill of Lading",
-                BillOfLadingV1::class,
-                false,
-                true
-            ),
             "business-card" => new DocumentCommandConfig(
                 "Business Card",
                 BusinessCardV1::class,
@@ -72,12 +57,6 @@ class MindeeCliDocuments
                 CropperV1::class,
                 true,
                 false
-            ),
-            "delivery-note" => new DocumentCommandConfig(
-                "Delivery note",
-                DeliveryNoteV1::class,
-                false,
-                true
             ),
             "driver-license" => new DocumentCommandConfig(
                 "Driver License",
@@ -103,35 +82,11 @@ class MindeeCliDocuments
                 true,
                 false
             ),
-            "fr-energy-bill" => new DocumentCommandConfig(
-                "FR Energy Bill",
-                EnergyBillV1::class,
-                false,
-                true
-            ),
-            "fr-health-card" => new DocumentCommandConfig(
-                "FR Health Card",
-                HealthCardV1::class,
-                false,
-                true
-            ),
             "fr-carte-nationale-d-identite" => new DocumentCommandConfig(
                 "FR Carte Nationale d'Identité",
                 IdCardV2::class,
                 true,
                 false
-            ),
-            "fr-payslip" => new DocumentCommandConfig(
-                "FR Payslip",
-                PayslipV3::class,
-                false,
-                true
-            ),
-            "ind-passport-india" => new DocumentCommandConfig(
-                "IND Passport - India",
-                IndianPassportV1::class,
-                false,
-                true
             ),
             "international-id" => new DocumentCommandConfig(
                 "International ID",
@@ -157,12 +112,6 @@ class MindeeCliDocuments
                 true,
                 false
             ),
-            "nutrition-facts-label" => new DocumentCommandConfig(
-                "Nutrition Facts Label",
-                NutritionFactsLabelV1::class,
-                false,
-                true
-            ),
             "passport" => new DocumentCommandConfig(
                 "Passport",
                 PassportV1::class,
@@ -186,18 +135,6 @@ class MindeeCliDocuments
                 BankCheckV1::class,
                 true,
                 false
-            ),
-            "us-healthcare-card" => new DocumentCommandConfig(
-                "US Healthcare Card",
-                HealthcareCardV1::class,
-                false,
-                true
-            ),
-            "us-us-mail" => new DocumentCommandConfig(
-                "US US Mail",
-                UsMailV3::class,
-                false,
-                true
             ),
         ];
     }
